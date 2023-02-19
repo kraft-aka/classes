08c4734062447fe5444845b6513eb100cdb26fd7
kubat <kubat.aiupov@gmail.com>
2023-02-19 22:26:22 +0100

    Initial commit

---
+++ classOne.js
@@ -0,0 +1,28 @@
+// class OOP
+
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get department() {
+    return this._department;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays = this._remainingVacationDays - daysOff;
+  }
+}
+
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+
+console.log(surgeonRomero.name)
+console.log(surgeonRomero.takeVacationDays(3))
+console.log(surgeonRomero.remainingVacationDays)
