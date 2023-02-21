72ae4ba21aa6e1d0b2f24af39770cf21009c0cac
kubat <kubat.aiupov@gmail.com>
2023-02-21 20:39:16 +0100

    add getter and method to child class

--- Nurse.js
+++ Nurse.js
@@ -5,10 +5,20 @@ class Nurse extends HospitalEmployee {
     super(name);
     this._certifications = certifications;
   }
+  // getter
+  get certifications() {
+    return this._certifications;
+  }
+  //method
+  addCertification(newCretification) {
+    this._certifications.push(newCretification);
+  }
 }
 
 const nurseOlynyk = new Nurse("Olynyk", ["Trauma", "Pediatrics"]);
 
 console.log(nurseOlynyk);
+nurseOlynyk.addCertification('Genetics')
+console.log(nurseOlynyk.certifications)
 
 // TODO : fix import
